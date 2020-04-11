8274cc43cc704b898a48718ec449735bcc369db9
Shantanu Joshi <weemadarthur@yggdrasil.in>
2020-04-11 08:34:26 +0530

    Add validation and media pipelines

---
+++ bragge/pipelines.py
@@ -0,0 +1,33 @@
+
+import scrapy
+
+class BraggeValidationPipeline():
+
+    def process_item(self, item, spider):
+
+        try:
+            for key in ('slug', 'url', 'title', 'synopsis'):
+                assert (key in item and len(item[key]) > 0), f'item does not have {key}'
+
+            assert 'date' in item and item['date'] is not None, 'item does not have date'
+
+            for key in ('links', 'reading_list'):
+                if key in item and len(item[key]) > 0:
+                    for entry in item[key]:
+                        assert len(entry) > 0, f'empty entry in {key}'
+
+            assert 'description' in item, 'missing description'
+            assert len(item['description']) > 0, 'empty description'
+            for entry in item['description']:
+                assert len(entry) > 0, f'empty entry in description'
+
+            assert len(item['files']) == 1 and len(item['files'][0]['path']) > 0, 'missing audio file'
+            assert len(item['images']) == 1 and len(item['images'][0]['path']) > 0, 'missing image'
+
+            return item
+
+        except AssertionError as ae:
+            for key,val in item.items():
+                item[key] = str(val)
+            spider.logger.error(f'Dropping item: {json.dumps(item)}')
+            raise scrapy.exceptions.DropItem(f'Validation Failure: {ae.args[0]}')
--- bragge/settings.py
+++ bragge/settings.py
@@ -1,10 +1,13 @@
 # Scrapy settings for bragge project
 
+import os
+
 BOT_NAME = 'bragge'
 SPIDER_MODULES = ['bragge.spiders']
 
 USER_AGENT = 'shiny-armadillo/0.1.0'
 ROBOTSTXT_OBEY = False
+MEDIA_ALLOW_REDIRECTS = True
 
 TELNETCONSOLE_ENABLED = False
 
@@ -14,3 +17,20 @@ EXTENSIONS = {
 CLOSESPIDER_ERRORCOUNT = 1
 CLOSESPIDER_ITEMCOUNT = 3
 CONCURRENT_REQUESTS = 1
+
+
+ITEM_PIPELINES = {
+    'scrapy.pipelines.files.FilesPipeline': 10,
+    'scrapy.pipelines.images.ImagesPipeline': 20,
+    'bragge.pipelines.BraggeValidationPipeline': 100,
+}
+
+BASEDIR = os.getenv('BRAGGE_BASEDIR', '/tmp/bragge')
+
+FILES_STORE  = os.path.join(BASEDIR, 'files/audio/')
+IMAGES_STORE = os.path.join(BASEDIR, 'files/images/')
+IMAGES_THUMBS = { 'small': (128, 72) }
+
+HTTPCACHE_ENABLED = True
+HTTPCACHE_DIR = os.path.join(BASEDIR, 'httpcache')
+HTTPCACHE_STORAGE = 'scrapy.extensions.httpcache.FilesystemCacheStorage'
